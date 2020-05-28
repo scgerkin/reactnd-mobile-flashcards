9d4fefffc5ac35cf90f72ff576cb98f83433b7eb
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-28 14:08:30 -0400

    Add statusbar

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View} from 'react-native'
+import FlashcardStatusBar from "./components/FlashcardStatusBar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <FlashcardStatusBar backgroundColor={"black"} barStyle={"light-content"}/>
+      <Text>App.js</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
