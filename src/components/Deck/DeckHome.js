c7e49fdef3b368fd38bcd6a479ad7504f986e372
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-30 14:49:35 -0400

    Clean code, convert to function

--- src/components/Deck/DeckHome.js
+++ src/components/Deck/DeckHome.js
@@ -1,56 +1,46 @@
 import React, {Fragment} from "react";
 import {connect} from "react-redux";
-import {ScrollView, Text, View} from "react-native";
+import {Text, View} from "react-native";
 import {createStackNavigator} from "@react-navigation/stack";
 import DeckDetails from "./DeckDetails";
-import DeckPreview from "./DeckPreview";
-import {deckPrimary, deckSecondary} from "../../styles/cards";
 import DeckList from "./DeckList";
 
 const Stack = createStackNavigator();
-export const DECK_ROOT = "Decks";
-export const DECK_DETAILS = "Deck Details";
 
 export const DECK_LIST = "DECK_LIST";
 
-class DeckHome extends React.Component {
+function DeckHome(props) {
+  const {decks} = props;
 
-  render() {
-    const {decks} = this.props;
-
-    if (!decks) {
-      return noDecks(this.props);
-    }
-
-    const deckIds = Object.keys(decks);
-
-    const {navigation} = this.props;
-
-    return (
-        <Fragment>
-          <Stack.Navigator initialRouteName={DECK_LIST}>
-            <Stack.Screen
-                name={DECK_LIST}
-                component={DeckList}
-                options={{title: "Your Decks"}}
-            />
-            {deckIds.map(id => {
-              const title = decks[id].title;
-              return (
-                  <Stack.Screen
-                      key={id}
-                      name={id}
-                      >
-                    {props => <DeckDetails {...props} deckId={id}/>}
-                  </Stack.Screen>);
-            })}
-          </Stack.Navigator>
-        </Fragment>
-    );
+  if (!decks) {
+    return noDecks(props);
   }
+
+  const deckIds = Object.keys(decks);
+
+  return (
+      <Fragment>
+        <Stack.Navigator initialRouteName={DECK_LIST}>
+          <Stack.Screen
+              name={DECK_LIST}
+              component={DeckList}
+              options={{title: "Your Decks"}}
+          />
+          {deckIds.map(id => {
+            const title = decks[id].title;
+            return (
+                <Stack.Screen
+                    key={id}
+                    name={id}
+                    options={{title: title}}
+                >
+                  {props => <DeckDetails {...props} deckId={id}/>}
+                </Stack.Screen>);
+          })}
+        </Stack.Navigator>
+      </Fragment>
+  );
 }
-
-
 
 function noDecks(props) {
   //todo handle no decks gracefully
