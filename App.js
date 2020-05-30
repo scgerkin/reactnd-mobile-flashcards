35c80ceb59bad02a51240dc6ae74b42b0782db67
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-30 16:15:07 -0400

    Clean and change to function

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import {StatusBar, Text, View} from 'react-native'
+import React, {Fragment} from "react";
+import {StatusBar, View} from "react-native";
 import Constants from "expo-constants";
 import {Provider} from "react-redux";
 import {createStore} from "redux";
-import reducer from "./src/redux/reducers"
-import middleware from "./src/redux/middleware"
+import reducer from "./src/redux/reducers";
+import middleware from "./src/redux/middleware";
 import MainDisplay from "./src/components/MainDisplay";
 import {CLR_GREY_LT} from "./src/styles/colors";
 
-const store = createStore(reducer, middleware)
+const store = createStore(reducer, middleware);
 
-class App extends React.Component {
-  render() {
-    return (
-        <Provider store={store}>
-          <View style={{flex: 1}}>
-            <FlashcardStatusBar backgroundColor={CLR_GREY_LT} barStyle={"light-content"}/>
-            <MainDisplay/>
-          </View>
-        </Provider>
-    );
-  }
+function App() {
+  return (
+      <Provider store={store}>
+        <Fragment>
+          <FlashcardStatusBar backgroundColor={CLR_GREY_LT}
+                              barStyle={"light-content"}/>
+          <MainDisplay/>
+        </Fragment>
+      </Provider>
+  );
 }
 
+//fixme using the height here pushes the header of the stack navigator down way
+// too far, but not using it makes add new question slide into the status bar
 function FlashcardStatusBar({backgroundColor, ...props}) {
   return (
       <View style={{backgroundColor, height: Constants.statusBarHeight}}>
@@ -30,6 +31,5 @@ function FlashcardStatusBar({backgroundColor, ...props}) {
       </View>
   );
 }
-
 
 export default App;
