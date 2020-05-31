45f6a80b084dfc61127dc0c23781b0d61dad21cc
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-31 16:34:20 -0400

    turn back on storage

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,9 @@
 import AsyncStorage from "@react-native-community/async-storage";
-import {testDecks} from "./_DATA";
 
 export const DECK_STORAGE_KEY = "com.scgrk.reactnd.mobileFlashCards::decks";
 
 export function fetchDecks() {
-  //fixme return storage after styling is done
-  return new Promise((res, rej) => {
-    res(JSON.stringify(testDecks))
-  });
-  //return AsyncStorage.getItem(DECK_STORAGE_KEY);
+  return AsyncStorage.getItem(DECK_STORAGE_KEY);
 }
 
 export function saveState(state) {
